911444431615cbbe164647d384842d819b0a8677
Aryeh Hillman <aryeh@looker.com>
2020-04-15 12:26:10 -0700

    Emit Correct CI Command

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ A simple chrome extension to make it easier to launch dialect tests
 
 ![Build](https://github.com/fzakaria/dialect-chrome-extension/workflows/Build/badge.svg?branch=master)
 
+## Adding Development Extension to Chrome
+
+1. Navigate to `chrome://extensions`
+2. Click to enable `Developer Mode`
+3. Click `Load Unpacked`
+
+Be sure existing development or production extension is not active. 
+
 ## Development
 
 ```bash
--- content.js
+++ content.js
@@ -1,42 +1,92 @@
+class CICommandProducer {
+    /**
+     *
+     * @param {Array.<string>} [connections] optional list of connections (e,g. `[denodo, exasol]`)
+     * @param testFile {string} optional test file to run (e.g. `test/unit/blah.js`)
+     * @param testRegexFilter {string} optional regex to limit tests that are run (e.g. `cool test foo`)
+     */
+    constructor(connections, testFile, testRegexFilter) {
+        this.connections = connections;
+        this.testFile = testFile;
+        this.testRegexFilter = testRegexFilter;
+    }
+
+    emitCommand() {
+        let cmd;
+        if (!this.connections || this.connections.length === 0) {
+            cmd = "CI:DIALECTS[";
+        } else {
+            cmd = "CI:DIALECT_TEST[(";
+            cmd += this.connections.join(" ");
+            cmd += ") ";
+        }
+
+        cmd += `TEST=\'${this.testFile}\' `;
+        cmd += `FILTER=/${this.testRegexFilter}/]`;
+        return cmd;
+    }
+}
+
+class PageRepresentation {
+    constructor() {}
+
+    get selectedDialects() {
+        let dialectSelectInput = document.getElementById("dialect-name-text");
+        let selectedDialects = [...dialectSelectInput.options]
+            .filter(option => option.selected)
+            .map(option => option.value);
+
+        if (
+            selectedDialects &&
+            selectedDialects.length === 1 &&
+            selectedDialects[0] === "all"
+        ) {
+            selectedDialects = [];
+        }
+
+        return selectedDialects;
+    }
+
+    get dialectTestFilename() {
+        return document
+            .getElementById("dialect-test-name")
+            .value.trim();
+    }
+
+    get dialectTestRegex() {
+        return document
+            .getElementById("dialect-test-regex")
+            .value.trim();
+    }
+
+    setComment(val) {
+        // get the DOM element for the <textarea> for the comment
+        let commentTextArea = document.getElementById("new_comment_field");
+        commentTextArea.value = val;
+    }
+}
+
 /*
  * This function is called when the modal dialogue for dialect tests is submitted.
  */
 function dialectTestFormSubmit(event) {
-  if (event) {
-    event.preventDefault();
-  }
-  // get the DOM element for the <textarea> for the comment
-  let commentTextArea = document.getElementById("new_comment_field");
+    if (event) {
+        event.preventDefault();
+    }
 
-  // collect all selected dialects from the select input
-  let dialectSelectInput = document.getElementById("dialect-name-text");
-  let selectedDialects = [...dialectSelectInput.options]
-    .filter(option => option.selected)
-    .map(option => option.value);
-  // if 'all' is any of the select dialects than don't use the special syntax to list it out
-  commentTextArea.value +=
-    `CI:DIALECTS` +
-    (selectedDialects.includes("all")
-      ? ""
-      : `[${selectedDialects.join(" ")}]`) +
-    "\n";
+    let pageRepresentation = new PageRepresentation();
 
-  let dialectTestFilename = document
-    .getElementById("dialect-test-name")
-    .value.trim();
-  let dialectTestRegex = document
-    .getElementById("dialect-test-regex")
-    .value.trim();
+    let command = new CICommandProducer(
+        pageRepresentation.selectedDialects,
+        pageRepresentation.dialectTestFilename,
+        pageRepresentation.dialectTestRegex
+    ).emitCommand();
 
-  if (dialectTestFilename.length !== 0) {
-    commentTextArea.value +=
-      `CI:DIALECT_TEST[TEST=${dialectTestFilename} FILTER=/${dialectTestRegex}/]` +
-      "\n";
-  }
+    pageRepresentation.setComment(command);
 
-  closeDialectModalDialog();
+    closeDialectModalDialog();
 
-  makeCommentButtonClickable();
+    makeCommentButtonClickable();
 }
 
 /*
@@ -46,30 +96,30 @@ function dialectTestFormSubmit(event) {
  * This just ends up doing the same code as toggle.
  */
 function closeDialectModalDialog() {
-  let dialectModalDialog = document.getElementById("dialect-modal-dialog");
-  dialectModalDialog.removeAttribute("open");
+    let dialectModalDialog = document.getElementById("dialect-modal-dialog");
+    dialectModalDialog.removeAttribute("open");
 }
 
 function makeCommentButtonClickable() {
-  let commentButton = Array.from(document.querySelectorAll("button")).find(
-    el => el.textContent.trim() === "Comment"
-  );
-  commentButton.removeAttribute("disabled");
+    let commentButton = Array.from(document.querySelectorAll("button")).find(
+        el => el.textContent.trim() === "Comment"
+    );
+    commentButton.removeAttribute("disabled");
 }
 
 const url = chrome.runtime.getURL("dialect.html");
 fetch(url)
-  .then(response => response.text())
-  .then(html => {
-    // include the HTML on the comment form
-    const commentFormActions = document.getElementById(
-      "partial-new-comment-form-actions"
-    );
-    const innerDivs = commentFormActions.getElementsByTagName("div");
-    innerDivs[0].insertAdjacentHTML("beforeend", html);
+    .then(response => response.text())
+    .then(html => {
+        // include the HTML on the comment form
+        const commentFormActions = document.getElementById(
+            "partial-new-comment-form-actions"
+        );
+        const innerDivs = commentFormActions.getElementsByTagName("div");
+        innerDivs[0].insertAdjacentHTML("beforeend", html);
 
-    // register the click button for the comment to close the modal dialogue
-    document
-      .getElementById("dialect-button-comment")
-      .addEventListener("click", dialectTestFormSubmit);
-  });
+        // register the click button for the comment to close the modal dialogue
+        document
+            .getElementById("dialect-button-comment")
+            .addEventListener("click", dialectTestFormSubmit);
+    });
